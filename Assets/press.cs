0f6645d9c1c4525031f2496cf3a2db27e535624b
poschanan <poschanan.p@gmail.com>
2016-03-20 18:20:23 +0700

    Fisrt Commit

---
+++ Assets/press.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class press : MonoBehaviour {
+	public Text txtPress;
+	public int count = 0;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Input.anyKeyDown) {
+			count++;
+			txtPress.text = "P R E S S : " + count;
+		}
+	}
+}
